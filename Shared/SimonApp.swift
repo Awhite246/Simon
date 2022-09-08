04039d116ae0061a1adfec869a1a7352885669a3
Alistiar White <awhite@bsd220.org>
2022-09-08 10:07:09 -0500

    Initial Commit

---
+++ Shared/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Shared
+//
+//  Created by Alistair White on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
