1516426cbf65b00b1897c9c575a1390c77dc450f
Alistiar White <awhite@bsd220.org>
2022-09-12 09:52:53 -0500

    Button struct

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,26 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var clickOrder = ""
+    @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
+    @State var correct = true
     var body: some View {
         VStack {
-            Text("Current Click Order")
-            Text(clickOrder)
-                .font(.caption)
-                .padding()
-            HStack {
-                ForEach((1...4), id: \.self) { num in
-                    Button(action: {
-                        clickOrder += "\(num)"
-                    }, label: {
-                        Text("\(num)")
-                            .padding()
-                            .font(.title)
-                    })
-                    
-                }
-            }
+            Text("Simon")
+                .font(.custom("Big Boy", size: 72))
         }
+        .preferredColorScheme(.dark)
+        .ignoresSafeArea()
+    }
+    
+}
+
+struct ColorDisplay: View {
+    let color: Color
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(color)
+            .frame(width: 100, height: 100, alignment: .center)
+            .padding()
     }
 }
 
